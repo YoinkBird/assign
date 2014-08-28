182709d7b6a063c9974d09962c958afc2015450d
Yoinkbird <yoinkbird2010@gmail.com>
2014-08-28 18:45:34 -0500

    refactor: removing old code and debug stubs

--- hw/hw1/testbench.py
+++ hw/hw1/testbench.py
@@ -6,50 +6,8 @@ verbose = 2
 #if(debug > 0):
 #perl-ish  verbose = 1 if not defined verbose
 
-def tableRowPrint(headerList):
-    #<table>
-
-    # rank is first cell, highest rank is 2 digits
-    # suit is second cell, longest suit is diamonds at index 2
-      # TODO no point in calculating right now...
-    cellWidthList = [2 , len(suitList[2])]
-    # but the len('rank') bigger than any rank
-    headerList = ['rank','suit']
-    if(len(headerList[0]) > cellWidthList[0]):
-      cellWidthList[0] = len(headerList[0])
-    # calculate cellpadding
-    getCellPaddingLam = lambda x,y: ' ' * (x - y)
-    
-    #< legacy>
-    #del tableHorizRow = '-' * tableWidth
-    #ver1 debugPrintStr = debugPrintStr + 'suit | rank' + '\n' + '----------' + '\n'
-    #ver2 debugPrintStr = debugPrintStr + 'suit | rank' + '\n' + tableHorizRow + '\n'
-    #ver3 debugPrintStr = debugPrintStr + ' suit ' + ' ' * (cellWidth - 4) + ' | rank' + '\n' + tableHorizRow + '\n'
-    #ver4 debugPrintStr = debugPrintStr + ' suit' + getCellPaddingLam(cellWidth , 4) + ' | rank' + '\n' + tableHorizRow + '\n'
-    #ver5 tableHeader = ' suit' + getCellPaddingLam(cellWidth , 4) + ' | rank' + '\n' + tableHorizRow + '\n'
-    #ver6 tableHeader = ' suit' + getCellPaddingLam(cellWidth , 4) + ' | rank ' 
-    #</legacy>
-
-    #ver7
-    # create padding for each cell
-    tableHeader  = headerList[0] + getCellPaddingLam(cellWidthList[0] , len(headerList[0]))
-    tableHeader += ' | ' 
-    tableHeader += headerList[1] + getCellPaddingLam(cellWidthList[1], len(headerList[1]))
-    tableHeader = ' ' + tableHeader + ' '
-    # horizontal line to delineate header and table begin,end
-    tableHorizRow = '-' * len(tableHeader)
-    tableHeader = tableHorizRow + '\n' + tableHeader + '\n' + tableHorizRow + '\n'
-    #< populate table>
-    table = ''
-    for index in deck:
-      #ver1 debugPrintStr = debugPrintStr + ' | '.join(index) + '\n'
-      #debugPrintStr = debugPrintStr + ' ' + index[0] + getCellPaddingLam(cellWidth,len(index[0])) + ' | ' + index [1] + '\n'
-      table = table + ' ' + index[0] + getCellPaddingLam(cellWidthList[0],len(index[0])) + ' | ' + index [1] + '\n'
-    #</populate table>
-
-    table = tableHeader + table + tableHorizRow
-    debugPrintStr = debugPrintStr + table + '\n'
-    # </table>
+#TODO: copy table code from initDeck 
+# def tableRowPrint(headerList):
 
 def initDeck(deckSize):
 
@@ -66,34 +24,12 @@ def initDeck(deckSize):
   suitList = ["spades", "hearts", "diamonds", "clubs"]
 
   # deck: list with 52 elements, each element is tuple with rank and suit
-  #deck = [("k","spades")]
   deck = []
-# populate deck:
-# loop 1 through 52 or 0 through 51 and choose rank, suit deterministically
-# deck[$index] = (rankList[$index % $len(rankList),suitList[$index % len(suitList)])
-# deck.append((rankList[len(rankList)%index],suitList[len(suitList)%index]))
-
-  # return 
-  #< legacy>
-  if(1):
-    getRandom = lambda list,index: list[ index % (len(list) - 1)]
-    # note: equ to 'for(index = 0, index < 52, index++)'
-    #deckSize = 52;
-    #for index in range(0,52):
-    for index in range(0,deckSize):
-      if(debug == 1):
-        print("index",index,"\n");
-      deck.append((getRandom(rankList,index),getRandom(suitList,index)))
-    #</legacy>
-
-    # found a better way to do it, remove the deck and start over
 
   # < deck generation>
-  # populate deck as if it were a real deck, no just with random cards :-)
+  # populate deck as if it were a real deck, not just with random cards :-)
   # populate deck with suit and rank
   if(1):
-    deck = []
-    #del(deck)
     for suitTmp in suitList:
       for rankTmp in rankList:
         deck.append((rankTmp,suitTmp))
@@ -108,10 +44,6 @@ def initDeck(deckSize):
     debugPrintStr = debugPrintStr + "total elements: " + str(len(deck)) + '\n'
   if(verbose == 2):
     #<table>
-    #print "total elements:" , len(deck)
-    # welp, print out a quick table I guess
-    #print "deck initialised:" , deck , "\n"
-    #tableWidth = 15
 
     # rank is first cell, highest rank is 2 digits
     # suit is second cell, longest suit is diamonds at index 2
@@ -124,16 +56,6 @@ def initDeck(deckSize):
     # calculate cellpadding
     getCellPaddingLam = lambda x,y: ' ' * (x - y)
     
-    #< legacy>
-    #del tableHorizRow = '-' * tableWidth
-    #ver1 debugPrintStr = debugPrintStr + 'suit | rank' + '\n' + '----------' + '\n'
-    #ver2 debugPrintStr = debugPrintStr + 'suit | rank' + '\n' + tableHorizRow + '\n'
-    #ver3 debugPrintStr = debugPrintStr + ' suit ' + ' ' * (cellWidth - 4) + ' | rank' + '\n' + tableHorizRow + '\n'
-    #ver4 debugPrintStr = debugPrintStr + ' suit' + getCellPaddingLam(cellWidth , 4) + ' | rank' + '\n' + tableHorizRow + '\n'
-    #ver5 tableHeader = ' suit' + getCellPaddingLam(cellWidth , 4) + ' | rank' + '\n' + tableHorizRow + '\n'
-    #ver6 tableHeader = ' suit' + getCellPaddingLam(cellWidth , 4) + ' | rank ' 
-    #</legacy>
-
     #ver7
     # create padding for each cell
     tableHeader  = headerList[0] + getCellPaddingLam(cellWidthList[0] , len(headerList[0]))
@@ -146,8 +68,6 @@ def initDeck(deckSize):
     #< populate table>
     table = ''
     for index in deck:
-      #ver1 debugPrintStr = debugPrintStr + ' | '.join(index) + '\n'
-      #debugPrintStr = debugPrintStr + ' ' + index[0] + getCellPaddingLam(cellWidth,len(index[0])) + ' | ' + index [1] + '\n'
       table = table + ' ' + index[0] + getCellPaddingLam(cellWidthList[0],len(index[0])) + ' | ' + index [1] + '\n'
     #</populate table>
 
@@ -178,34 +98,29 @@ gofishDeck = initDeck(52)
 from gofish1 import getCard
 from gofish1 import drawCard
 
-#awww crap...card1 = getCard(gofishDeck)
-
-# invent some players
+# invent some players - for now only one is needed
 playerList = ["johny","barker","sally","sushmita"]
 playerHand = {};
 #TODO : create a player class and instantiate 4 players
-debugDeck = []
+
 #for i in range(0,52): ## 52 is one longer, causes an error, and therefore rules out incomplete loop as the cause
 while gofishDeck:
-  debugDeck.append(drawCard(playerList[0], gofishDeck, playerHand))
+  drawCard(playerList[0], gofishDeck, playerHand)
 
-pprint(sorted(debugDeck)) 
-
-#for player in playerList:
-#  drawCard(player,gofishDeck,playerHand)
 from pprint import pprint
 
-#print("for-loop printout")
-print("final hand")
-for ranks in sorted(playerHand):
-  #print(ranks, sorted(playerHand[ranks]))
-  playerHand[ranks] = sorted(playerHand[ranks])
+pprint(playerHand,indent=2)
 
-pprint(playerHand, indent = 1)
-
-import json
+# technique for printing dict as indented structure
 if(0):
+  import json
   print json.dumps(playerHand, sort_keys=True, indent=4)
 
 # leave this here for debugger to run to
 print
+
+
+### tips
+## print without newline. only accepts one arg, no lists etc.
+  #import sys
+  # sys.stdout.write()
