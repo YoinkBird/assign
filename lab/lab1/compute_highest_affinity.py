484e05ae7c9b6b73c9b705dfd50d75468a694180
Yoinkbird <yoinkbird2010@gmail.com>
2014-08-29 18:49:22 -0500

    lab1.1 update: added def to store log data in a dict

--- lab/lab1/compute_highest_affinity.py
+++ lab/lab1/compute_highest_affinity.py
@@ -5,8 +5,61 @@
 
 # See the test cases for more details.
 
+from pprint import pprint
+verbose = 0
+
+################################################################
+
+def collate_data(site_list, user_list, time_list):
+  # 1. Check that input data is sane
+  #  i.e. all lists are same length
+  # 2. Populate a dict with data per website
+  # webLogDict[site][user][[time]]
+  # dict for sites
+  #   dict for users
+  #     list for times
+
+  # hold message and return value
+  retStr = ""
+  retVal = 1
+
+  webLogDict = {}
+  # check that all three lists are same length
+  if(len(site_list) != len(user_list) != len(time_list)):
+    retStr = "input list mismatch."
+    retVal = 1
+  else:
+    # loop through all three lists and populate the webLogDict
+    for index in range(0,len(site_list)):
+      tmpSite = site_list[index]
+      tmpUser = user_list[index]
+      tmpTime = time_list[index]
+      # define key:site as hash if not present
+      if (tmpSite not in webLogDict):
+        webLogDict[tmpSite] = {}
+      # define key:user as array if not present. user-array for timestamps
+      if (tmpUser not in webLogDict[tmpSite]):
+        webLogDict[tmpSite][tmpUser] = []
+      # add timestamp array if not present
+      webLogDict[tmpSite][tmpUser].append([tmpTime])
+      #else:
+        #webLogDict[tmpSite]
+
+  if(verbose >= 1):
+    pprint(webLogDict)
+  return webLogDict
+#</def collate_data> 
+
 def highest_affinity(site_list, user_list, time_list):
   # Returned string pair should be ordered by dictionary order
   # I.e., if the highest affinity pair is "foo" and "bar"
   # return ("bar", "foo"). 
+
+  # add data to a dict
+  collate_data(site_list, user_list, time_list)
+
   return ('abc', 'def')
+
+
+# idea:
+# dict with key of site, dict[site].append(user) and then find max len (dict[site})
