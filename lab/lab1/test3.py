765c6aff188686bc13204de29bc1242e37eb8ee7
Yoinkbird <yoinkbird2010@gmail.com>
2014-08-30 20:38:36 -0500

    lab1p1: updating tests to fix bug in test3 and make test consistent in printing to screen

--- lab/lab1/test3.py
+++ lab/lab1/test3.py
@@ -22,9 +22,10 @@ H = "H"
 user_list = [A, C, H, A, C, D, E, F, G, C, D, E, F, G, H, C, B, D, E, F, G, C, G, A, D, E, H, A, C, E, H]
 site_list = [a, a, a, b, b, b, b, b, b, c, c, c, c, c, c, d, e, e, e, e, e, f, f, g, g, g, g, h, h, h, h]
 
-time_list = range(0,30)
+time_list = range(0,len(user_list))
 
 computed_result = compute_highest_affinity.highest_affinity(site_list, user_list, time_list)
 expected_result = (b, c)
 
 assert computed_result == expected_result
+print "Successfully passed test3!"
